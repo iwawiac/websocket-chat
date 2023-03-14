a46a52ecebc7d9ccb5bc2d9908f80214b6179ae5
Slava Ivantsou <iwawiac@gmail.com>
2023-03-14 20:15:42 +0100

    Replaced old Java IO library with JAVA NIO library in WebSocketListener

--- chat-client/src/main/java/pl/sages/chat/client/WebSocketListener.java
+++ chat-client/src/main/java/pl/sages/chat/client/WebSocketListener.java
@@ -1,16 +1,18 @@
 package pl.sages.chat.client;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.http.WebSocket;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.CompletionStage;
 
 public class WebSocketListener implements WebSocket.Listener {
 
     private String tempFileName = "temp";
-    private String userName;
+    private final String userName;
 
     public WebSocketListener(String userName) {
         this.userName = userName;
@@ -32,14 +34,10 @@ public class WebSocketListener implements WebSocket.Listener {
         return WebSocket.Listener.super.onText(webSocket, data, last);
     }
 
-    @Override
     public CompletionStage<?> onBinary(WebSocket webSocket, ByteBuffer data, boolean last) {
-        File file = new File(System.getProperty("user.dir"), userName+"_downloaded_" + tempFileName);
-        try(FileOutputStream fileOutputStream = new FileOutputStream(file, true)) {
-            while (data.hasRemaining()) {
-                fileOutputStream.getChannel().write(data);
-                fileOutputStream.close();
-            }
+        Path path = Paths.get(System.getProperty("user.dir"), userName + "_downloaded_" + tempFileName);
+        try (FileChannel channel = FileChannel.open(path, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND)) {
+            channel.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
